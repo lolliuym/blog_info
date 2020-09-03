5e09637ebf06107b6a3d12e4a4540c21e265d30f
Valery Anestiadis <anest12mmm@gmail.com>
2020-09-03 16:41:52 +0300

    change

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,5 +1,10 @@
 import '../styles/main.scss'
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
 
+import { faCommentDots, faEllipsisH } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCommentDots, faEllipsisH)
+dom.watch()
 if (process.env.NODE_ENV !== 'production') {
   require('../index.pug')
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require('path')
+const path = require("path");
 
-const glob = require('glob')
-const webpack = require('webpack')
-const merge = require('webpack-merge')
-const HtmlPlugin = require('html-webpack-plugin')
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-const StylelintPlugin = require('stylelint-webpack-plugin')
-const ManifestPlugin = require('webpack-manifest-plugin')
-const CleanPlugin = require('clean-webpack-plugin')
-const { StatsWriterPlugin } = require('webpack-stats-plugin')
+const glob = require("glob");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const HtmlPlugin = require("html-webpack-plugin");
+const FriendlyErrorsPlugin = require("friendly-errors-webpack-plugin");
+const StylelintPlugin = require("stylelint-webpack-plugin");
+const ManifestPlugin = require("webpack-manifest-plugin");
+const CleanPlugin = require("clean-webpack-plugin");
+const { StatsWriterPlugin } = require("webpack-stats-plugin");
 
-const parts = require('./webpack.parts')
+const parts = require("./webpack.parts");
 
 const lintJSOptions = {
   emitWarning: true,
@@ -22,8 +22,8 @@ const lintJSOptions = {
   fix: true,
   cache: true,
 
-  formatter: require('eslint-friendly-formatter')
-}
+  formatter: require("eslint-friendly-formatter"),
+};
 
 /*
   To move all assets to some static folder
@@ -45,79 +45,79 @@ const lintJSOptions = {
            css - 'styles',
             js - 'scripts'
 */
-const paths = getPaths()
+const paths = getPaths();
 
 const lintStylesOptions = {
   context: path.resolve(__dirname, `${paths.app}/styles`),
-  syntax: 'scss',
-  emitErrors: false
+  syntax: "scss",
+  emitErrors: false,
   // fix: true,
-}
+};
 
-const cssPreprocessorLoader = { loader: 'fast-sass-loader' }
+const cssPreprocessorLoader = { loader: "fast-sass-loader" };
 
 const commonConfig = merge([
   {
     context: paths.app,
     resolve: {
       unsafeCache: true,
-      symlinks: false
+      symlinks: false,
     },
     entry: `${paths.app}/scripts`,
     output: {
       path: paths.build,
-      publicPath: parts.publicPath
+      publicPath: parts.publicPath,
     },
     stats: {
-      warningsFilter: warning => warning.includes('entrypoint size limit'),
+      warningsFilter: (warning) => warning.includes("entrypoint size limit"),
       children: false,
-      modules: false
+      modules: false,
     },
     plugins: [
       new HtmlPlugin({
-        template: './index.pug'
+        template: "./index.pug",
       }),
       new FriendlyErrorsPlugin(),
-      new StylelintPlugin(lintStylesOptions)
+      new StylelintPlugin(lintStylesOptions),
     ],
     module: {
-      noParse: /\.min\.js/
-    }
+      noParse: /\.min\.js/,
+    },
   },
   parts.loadPug(),
   parts.lintJS({ include: paths.app, options: lintJSOptions }),
   parts.loadFonts({
     include: paths.app,
     options: {
-      name: `${paths.fonts}/[name].[hash:8].[ext]`
-    }
-  })
-])
+      name: `${paths.fonts}/[name].[hash:8].[ext]`,
+    },
+  }),
+]);
 
 const productionConfig = merge([
   {
-    mode: 'production',
+    mode: "production",
     optimization: {
       splitChunks: {
-        chunks: 'all'
+        chunks: "all",
       },
-      runtimeChunk: 'single'
+      runtimeChunk: "single",
     },
     output: {
       chunkFilename: `${paths.js}/[name].[chunkhash:8].js`,
-      filename: `${paths.js}/[name].[chunkhash:8].js`
+      filename: `${paths.js}/[name].[chunkhash:8].js`,
     },
     performance: {
-      hints: 'warning', // 'error' or false are valid too
+      hints: "warning", // 'error' or false are valid too
       maxEntrypointSize: 100000, // in bytes
-      maxAssetSize: 450000 // in bytes
+      maxAssetSize: 450000, // in bytes
     },
     plugins: [
-      new StatsWriterPlugin({ fields: null, filename: '../stats.json' }),
+      new StatsWriterPlugin({ fields: null, filename: "../stats.json" }),
       new webpack.HashedModuleIdsPlugin(),
       new ManifestPlugin(),
-      new CleanPlugin()
-    ]
+      new CleanPlugin(),
+    ],
   },
   parts.minifyJS({
     terserOptions: {
@@ -127,7 +127,7 @@ const productionConfig = merge([
         // into invalid ecma 5 code. This is why the 'compress' and 'output'
         // sections only apply transformations that are ecma 5 safe
         // https://github.com/facebook/create-react-app/pull/4234
-        ecma: 8
+        ecma: 8,
       },
       compress: {
         ecma: 5,
@@ -136,103 +136,106 @@ const productionConfig = merge([
         // https://github.com/facebook/create-react-app/issues/2376
         // Pending further investigation:
         // https://github.com/mishoo/UglifyJS2/issues/2011
-        comparisons: false
+        comparisons: false,
       },
       mangle: {
-        safari10: true
+        safari10: true,
       },
       output: {
         ecma: 5,
         comments: false,
         // Turned on because emoji and regex is not minified properly using default
         // https://github.com/facebook/create-react-app/issues/2488
-        ascii_only: true
-      }
+        ascii_only: true,
+      },
     },
     // Use multi-process parallel running to improve the build speed
     // Default number of concurrent runs: os.cpus().length - 1
     parallel: true,
     // Enable file caching
-    cache: true
+    cache: true,
   }),
   parts.loadJS({
     include: paths.app,
     options: {
-      cacheDirectory: true
-    }
+      cacheDirectory: true,
+    },
   }),
   parts.extractCSS({
     include: paths.app,
     use: [parts.autoprefix(), cssPreprocessorLoader],
     options: {
       filename: `${paths.css}/[name].[contenthash:8].css`,
-      chunkFilename: `${paths.css}/[id].[contenthash:8].css`
-    }
+      chunkFilename: `${paths.css}/[id].[contenthash:8].css`,
+    },
   }),
   parts.purifyCSS({
     paths: glob.sync(`${paths.app}/**/*.+(pug|js)`, { nodir: true }),
-    styleExtensions: ['.css', '.scss']
+    styleExtensions: [".css", ".scss"],
   }),
   parts.minifyCSS({
     options: {
       discardComments: {
-        removeAll: true
-      }
-    }
+        removeAll: true,
+      },
+    },
   }),
   parts.loadImages({
     include: paths.app,
     options: {
       limit: 15000,
-      name: `${paths.images}/[name].[hash:8].[ext]`
-    }
+      name: `${paths.images}/[name].[hash:8].[ext]`,
+    },
   }),
   // should go after loading images
-  parts.optimizeImages()
-])
+  parts.optimizeImages(),
+]);
 
 const developmentConfig = merge([
   {
-    mode: 'development'
+    mode: "development",
   },
   parts.devServer({
     host: process.env.HOST,
-    port: process.env.PORT
+    port: process.env.PORT,
   }),
   parts.loadCSS({ include: paths.app, use: [cssPreprocessorLoader] }),
   parts.loadImages({ include: paths.app }),
-  parts.loadJS({ include: paths.app })
-])
+  parts.loadJS({ include: paths.app }),
+]);
 
-module.exports = env => {
-  process.env.NODE_ENV = env
+module.exports = (env) => {
+  process.env.NODE_ENV = env;
 
   return merge(
     commonConfig,
-    env === 'production' ? productionConfig : developmentConfig
-  )
-}
+    env === "production" ? productionConfig : developmentConfig
+  );
+};
 
-function getPaths ({
-  sourceDir = 'app',
-  buildDir = 'build',
-  staticDir = '',
-  images = 'images',
-  fonts = 'fonts',
-  js = 'scripts',
-  css = 'styles'
+function getPaths({
+  sourceDir = "app",
+  buildDir = "build",
+  staticDir = "",
+  images = "images",
+  fonts = "fonts",
+  js = "scripts",
+  css = "styles",
 } = {}) {
-  const assets = { images, fonts, js, css }
+  const assets = { images, fonts, js, css };
 
-  return Object.keys(assets).reduce((obj, assetName) => {
-    const assetPath = assets[assetName]
+  return Object.keys(assets).reduce(
+    (obj, assetName) => {
+      const assetPath = assets[assetName];
 
-    obj[assetName] = !staticDir ? assetPath : `${staticDir}/${assetPath}`
+      obj[assetName] = !staticDir ? assetPath : `${staticDir}/${assetPath}`;
 
-    return obj
-  }, {
-    app: path.join(__dirname, sourceDir),
-    build: path.join(__dirname, buildDir),
-    staticDir
-  })
+      return obj;
+    },
+    {
+      app: path.join(__dirname, sourceDir),
+      build: path.join(__dirname, buildDir),
+      staticDir,
+    }
+  );
 }
